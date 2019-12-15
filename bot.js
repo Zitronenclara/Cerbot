8d9f449a1da9ecd4e03a752e93e4cda4ac366198
unknown <universequru@gmail.com>
2019-12-15 16:27:21 +0100

    Cerbot v1.7

--- bot.js
+++ bot.js
@@ -334,7 +334,7 @@ let dailygain = function (us, ch, usid, tstamp) {
 exports.dailygain = dailygain;
 
 let logging = function (color, tit, desc, ftit, fdesc) {
-	const logch = client.channels.get("634011755021991961")
+	const logch = client.channels.get("633308005630476308")
 	const logmes = new Discord.RichEmbed()
 		.setColor(color)
 		.setTitle(tit)
